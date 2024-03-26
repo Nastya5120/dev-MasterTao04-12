74fcac2a8670af3bb9a6755699439c8c33f11d55
Nastya <nkotelevets0@gmail.com>
2024-03-26 14:29:15 +0200

    New first-screen

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger')
 const headerNav = document.querySelector('.header-nav')
 
+
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('is-active')
     headerNav.classList.toggle('show')
@@ -8,35 +9,17 @@ hamburger.addEventListener('click', function() {
 
 
 $('#first-screen-slider').slick({
-    dots: true,
-    appendArrows: $('.first-screen-arrows'),
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
+    arrows:true,
+    dots:true,
+    appendArrows:$('.first-screen-arrow'),
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
             arrows: false,
-        }
-    }, ]
-});
+          }
+        },
+      ]
+    });
 
-
-$('select').niceSelect();
-
-const fileInput = document.querySelector('#calculation-file');
-const fileName = document.querySelector('#file-name')
-
-fileInput.addEventListener('change', () => {
-    fileName.innerHTML = fileInput.files[0].name;
-});
-
-
-const controledField = document.querySelectorAll('[type="checkbox"]')
-const fileField = document.querySelectorAll('[type="file"]')
-
-controledField.forEach(input => {
-    input.closest('.input-wrapper').classList.add('controled-wrapper')
-    input.closest('.col').classList.add('controled-col')
-})
-
-fileField.forEach(input => {
-  input.closest('.input-wrapper').classList.add('file-wrapper')
-})
+  
